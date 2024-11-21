5b9c1d203ff1503f64cc04e84158c1fe6956a87d
Sujith P <sujithp2690@gmail.com>
2024-11-21 09:57:41 +0530

    profile

--- controller/profileController.js
+++ controller/profileController.js
@@ -8,16 +8,18 @@ export const profileUpdate = async (req, res) => {
         if (!userId) {
             return res.status(400).json({ message: "Invalid token required" });
         }
+
         let updatedUser = await UserModel.findOneAndUpdate({ _id: userId, email }, { userName }, { new: true });
         if (!updatedUser) {
             updatedUser = await AdminModel.findOneAndUpdate({ _id: userId, email }, { userName }, { new: true });
         }
+
         if (!updatedUser) {
             return res.status(404).json({ message: "User not found" });
         }
         res.status(200).json({ message: "Profile updated successfully", user: updatedUser });
     } catch (error) {
         console.error("Error updating profile:", error);
-        res.status(500).json({ message: "Internal server error", error: error.message });
+        res.status(500).json({ message: " Internal server error", error: error.message });
     }
 };
